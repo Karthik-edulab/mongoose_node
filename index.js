ad2863c41f6305f0a735d7e9c58e1a7ce945b3b5
karthik <karthik@edulab.in>
2022-12-28 16:37:22 +0530

    kg-feat-crud-mongoose

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
  
- 
-const mongoose=require('mongoose');
-const main= async()=>{
-    await mongoose.connect("mongodb://localhost:27017/e-com");
-    const ProductSchema= new mongoose.Schema({
-        brand:String
+ // connect with mongoose
+    const mongoose=require('mongoose');
+    mongoose.connect("mongodb://localhost:27017/e-com");
+    const ProductSchema= new mongoose.Schema({            //putting restriction
+        brand:String,
+        price:Number
     });
-
-
-
+     
+    // insert the data
+    const insert=async ()=>{
     const productsModel=mongoose.model('products',ProductSchema);
-    let data=new productsModel({brand:"motorola"});
-    let result=await data.save();
+    let data=new productsModel({
+        brand:"motorola"
+      });
+    let result=await  data.save();
     console.log(result)
-}
-main();
+    } 
+    insert();
+
+    //update the data
+    const updateInDb=async ()=>{
+    const product=mongoose.model('products',ProductSchema);
+    let data= await product.updateOne({price:1500},{$set:{brand: "Redmi"}})
+    console.log(data);
+    }
+    updateInDb();
+
+    //delete the data
+    const deleteInDb=async ()=>{
+    const product=mongoose.model('products',ProductSchema);
+    let data= await product.updateOne({brand:"motorola"} )
+    console.log(data);
+    }
+    deleteInDb()
+
+    //read the data 
+    const findInDb=async ()=>{
+    const product=mongoose.model('products',ProductSchema);
+    let data= await product.find( )
+    console.log(data);
+    }
+    findInDb();
